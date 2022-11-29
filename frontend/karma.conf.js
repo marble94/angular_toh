ed03c90f9dbd74dc9bb92e65e3a76e8c38d363c6
Marvin Blessing <m-blessing@freenet.de>
2022-11-29 13:50:03 +0100

    Add coverage output and build step.

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,8 +25,9 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        {type: 'html'},
+        {type: 'text-summary'},
+        {type: 'lcovonly'},
       ]
     },
     reporters: ['progress', 'kjhtml'],
